977ad7531130f1cebe03c695fa8ccd99ccf9dbe6
foree <beijing.2008.lm@163.com>
2016-08-18 21:13:38 +0800

    添加主界面的跳转到具体列表

--- app/src/main/java/org/foree/zetianji/ui/activity/BookShelfActivity.java
+++ app/src/main/java/org/foree/zetianji/ui/activity/BookShelfActivity.java
@@ -1,15 +1,27 @@
 package org.foree.zetianji.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import org.foree.zetianji.R;
 
-public class BookShelfActivity extends AppCompatActivity {
-
+public class BookShelfActivity extends AppCompatActivity implements CardView.OnClickListener{
+    CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_shelf);
+
+        cardView = (CardView) findViewById(R.id.novel_card);
+        cardView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(BookShelfActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
