b42271204053bca541949870be8eef09f55df1ae
foree <beijing.2008.lm@163.com>
2017-01-09 13:40:31 +0800

    增加解析BookInfo

--- app/src/main/java/org/foree/bookreader/website/BiQuGeWebInfo.java
+++ app/src/main/java/org/foree/bookreader/website/BiQuGeWebInfo.java
@@ -5,11 +5,13 @@ import android.util.Log;
 import org.foree.bookreader.book.Article;
 import org.foree.bookreader.book.Book;
 import org.foree.bookreader.book.Chapter;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -50,7 +52,52 @@ public class BiQuGeWebInfo extends WebInfo {
 
     @Override
     Book parseBookInfo(Document doc) {
-        return null;
+        Book book = new Book();
+        Chapter newestChapter = new Chapter();
+
+        Elements updates = doc.select("[property~=og:book*]");
+        for(Element update: updates){
+            Log.i(TAG, update.toString());
+            switch (update.attr("property")){
+                case "og:book:category":
+                    book.setCategory(update.attr("content"));
+                    break;
+                case "og:book:author":
+                    book.setAuthor(update.attr("content"));
+                    break;
+                case "og:book:book_name":
+                    book.setBook_name(update.attr("content"));
+                    break;
+                case "og:book:update_time":
+                    book.setUpdate_time(update.attr("content"));
+                    break;
+                case "og:book:latest_chapter_name":
+                    newestChapter.setTitle(update.attr("content"));
+                    break;
+                case "og:book:latest_chapter_url":
+                    newestChapter.setUrl(update.attr("content"));
+                    break;
+            }
+        }
+        book.setNewest_chapter(newestChapter);
+
+        // ChapterList
+        List<Chapter> chapters = new ArrayList<>();
+        Elements elements_contents = doc.select("dd");
+        Document contents = Jsoup.parse(elements_contents.toString());
+        Elements elements_a = contents.getElementsByTag("a");
+        for(Element link: elements_a){
+            Chapter chapter = new Chapter();
+            chapter.setTitle(link.text());
+            chapter.setUrl(url + link.attr("href"));
+            //Log.i("HH", link.text());
+            //Log.i("HH", link.attr("href"));
+            chapters.add(chapter);
+        }
+        Collections.reverse(chapters);
+        book.setChapter_list(chapters);
+
+        return book;
     }
 
     @Override
