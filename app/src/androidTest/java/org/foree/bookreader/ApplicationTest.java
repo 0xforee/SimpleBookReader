05deb8889768d2c6e657bc217f8754ecb2b7877e
foree <beijing.2008.lm@163.com>
2017-01-10 17:03:50 +0800

    增加一些测试代码

--- app/src/androidTest/java/org/foree/bookreader/ApplicationTest.java
+++ app/src/androidTest/java/org/foree/bookreader/ApplicationTest.java
@@ -3,11 +3,55 @@ package org.foree.bookreader;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import org.foree.bookreader.book.Book;
+import org.foree.bookreader.dao.BookDao;
+import org.foree.bookreader.net.NetCallback;
+import org.foree.bookreader.website.BiQuGeWebInfo;
+import org.foree.bookreader.website.WebInfo;
+
+import java.util.List;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+
+    }
+
+    public void testAddBookUnique(){
+        BookDao bookDao = new BookDao(getContext());
+
+        // 测试book_url是不是unique
+        Book book = new Book("hahah1", "http://m.bxwx9.org/b/98/98289/");
+        Book book1 = new Book("hahahh2", "http://m.bxwx9.org/b/98/98289/");
+        bookDao.addBookInfo(book);
+        bookDao.addBookInfo(book1);
+
+        List<Book> bookList =  bookDao.findAllBookList();
+        assertEquals(bookList.size(), 4);
+    }
+
+    public void testRemoveBook(){
+        BookDao bookDao = new BookDao(getContext());
+
+        String book_url = "http://m.bxwx9.org/b/98/98289/";
+        bookDao.removeBookInfo(book_url);
+    }
+
+    public void testParseBookInfo(){
+        WebInfo webInfo = new BiQuGeWebInfo();
+        webInfo.getBookInfo("http://www.biquge.com/0_168/", new NetCallback<Book>() {
+            @Override
+            public void onSuccess(Book data) {
+
+            }
+
+            @Override
+            public void onFail(String msg) {
+
+            }
+        });
     }
 }
--- app/src/test/java/org/foree/bookreader/ExampleUnitTest.java
+++ app/src/test/java/org/foree/bookreader/ExampleUnitTest.java
@@ -12,4 +12,16 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void convertUrl2Id(){
+        String url = "/0_168/2512063.html";
+        String url2 = "http://m.bxwx9.org/b/98/98289//0_168/2512063.html";
+        String id = null;
+
+        String[] subString = url2.split("/|\\.");
+        id = subString[subString.length-2];
+
+        assertEquals(id, "2512063");
+    }
 }
