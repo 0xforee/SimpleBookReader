fd3a31a721b385f281e4ac53333c1e4420867e9c
foree <beijing.2008.lm@163.com>
2017-02-09 21:28:04 +0800

    1. bookInfo页面加入进度条显示 2. 加入书架按钮增加用户提示

--- app/src/main/java/org/foree/bookreader/ui/activity/BookInfoActivity.java
+++ app/src/main/java/org/foree/bookreader/ui/activity/BookInfoActivity.java
@@ -9,7 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.foree.bookreader.R;
@@ -31,6 +33,13 @@ public class BookInfoActivity extends AppCompatActivity {
     private BookDao bookDao;
     private Toolbar toolbar;
     private Book book;
+    private LinearLayout linearLayout;
+
+    private static final int STATE_FAILED = -1;
+    private static final int STATE_LOADING = 0;
+    private static final int STATE_SUCCESS = 1;
+
+    private ProgressBar progressBar;
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -50,6 +59,8 @@ public class BookInfoActivity extends AppCompatActivity {
         bookUrl = bundle.getString("book_url");
 
         setupView();
+
+        notifyUpdate(STATE_LOADING);
     }
 
     private void setupLayout() {
@@ -60,6 +71,8 @@ public class BookInfoActivity extends AppCompatActivity {
         tvNovelName = (TextView) findViewById(R.id.tv_novel_name);
         tvNovelAuthor = (TextView) findViewById(R.id.tv_novel_author);
         tvNovelDescription = (TextView) findViewById(R.id.tv_description);
+        progressBar = (ProgressBar) findViewById(R.id.pb_progress);
+        linearLayout = (LinearLayout)findViewById(R.id.ll_book_info);
         bt = (Button) findViewById(R.id.bt_add);
         lv = (ListView) findViewById(R.id.lv_chapter_list);
 
@@ -67,10 +80,33 @@ public class BookInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 bookDao.addBookInfo(book);
+
+                // update bt_add
+                bt.setBackgroundColor(getResources().getColor(R.color.material_drawer_dark_hint_text));
+                bt.setText(getResources().getText(R.string.bt_added));
+                bt.setClickable(false);
             }
         });
     }
 
+    private void notifyUpdate(int state){
+        switch (state){
+            case STATE_FAILED:
+                break;
+            case STATE_LOADING:
+                progressBar.setVisibility(View.VISIBLE);
+                linearLayout.setVisibility(View.INVISIBLE);
+                tvNovelDescription.setVisibility(View.INVISIBLE);
+                break;
+            case STATE_SUCCESS:
+                progressBar.setVisibility(View.GONE);
+                linearLayout.setVisibility(View.VISIBLE);
+                tvNovelDescription.setVisibility(View.VISIBLE);
+
+                break;
+
+        }
+    }
     private void setupView() {
         AbsWebParser webinfo = WebParserManager.getInstance().getWebParser(bookUrl);
         webinfo.getBookInfo(bookUrl, new NetCallback<Book>() {
@@ -83,7 +119,7 @@ public class BookInfoActivity extends AppCompatActivity {
                         tvNovelName.setText(data.getBookName());
                         tvNovelAuthor.setText(data.getAuthor());
                         tvNovelDescription.setText(Html.fromHtml(data.getDescription()));
-                        bt.setClickable(true);
+                        notifyUpdate(STATE_SUCCESS);
                     }
                 }, 0);
 
