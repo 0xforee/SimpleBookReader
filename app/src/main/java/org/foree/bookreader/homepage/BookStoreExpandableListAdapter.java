bc9f7e9367e32e13c7f622d6bd923ef2ac6712bc
foree <daniel2008lee@163.com>
2017-03-11 17:20:17 +0800

    书城页使用expandableListView存放数据

---
+++ app/src/main/java/org/foree/bookreader/homepage/BookStoreExpandableListAdapter.java
@@ -0,0 +1,154 @@
+package org.foree.bookreader.homepage;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.TextView;
+
+import org.foree.bookreader.R;
+import org.foree.bookreader.bean.book.Book;
+
+import java.util.List;
+
+/**
+ * Created by foree on 17-3-11.
+ */
+
+public class BookStoreExpandableListAdapter implements ExpandableListAdapter {
+    private List<List<Book>> bookStoreList;
+    private Context mContext;
+
+    public BookStoreExpandableListAdapter(Context context, List<List<Book>> bookStoreList) {
+        mContext = context;
+        this.bookStoreList = bookStoreList;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getGroupCount() {
+        return bookStoreList.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return bookStoreList.get(groupPosition).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return bookStoreList.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return bookStoreList.get(groupPosition).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        GroupViewHolder groupViewHolder;
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.book_store_category, null);
+            groupViewHolder = new GroupViewHolder(convertView);
+            convertView.setTag(groupViewHolder);
+        } else {
+            groupViewHolder = (GroupViewHolder) convertView.getTag();
+        }
+        groupViewHolder.textView.setText(bookStoreList.get(groupPosition).get(0).getBookUrl());
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ChildViewHolder childViewHolder;
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.book_store_item, null);
+            childViewHolder = new ChildViewHolder(convertView);
+            convertView.setTag(childViewHolder);
+        } else {
+            childViewHolder = (ChildViewHolder) convertView.getTag();
+        }
+        childViewHolder.textView.setText(bookStoreList.get(groupPosition).get(childPosition).getBookName());
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void onGroupExpanded(int groupPosition) {
+
+    }
+
+    @Override
+    public void onGroupCollapsed(int groupPosition) {
+
+    }
+
+    @Override
+    public long getCombinedChildId(long groupId, long childId) {
+        return 0;
+    }
+
+    @Override
+    public long getCombinedGroupId(long groupId) {
+        return 0;
+    }
+
+    private static class GroupViewHolder {
+        private TextView textView;
+
+        GroupViewHolder(View view) {
+            textView = (TextView) view.findViewById(R.id.book_store_category_title);
+        }
+    }
+
+    private static class ChildViewHolder {
+        private TextView textView;
+
+        ChildViewHolder(View view) {
+            textView = (TextView) view.findViewById(R.id.book_store_item_name);
+        }
+    }
+}
--- app/src/main/java/org/foree/bookreader/homepage/BookStoreFragment.java
+++ app/src/main/java/org/foree/bookreader/homepage/BookStoreFragment.java
@@ -2,13 +2,25 @@ package org.foree.bookreader.homepage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 
 import org.foree.bookreader.R;
+import org.foree.bookreader.bean.book.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookStoreFragment extends Fragment {
+    private ExpandableListView mExpandableListView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private ExpandableListAdapter mAdapter;
+    private List<List<Book>> bookStoreList;
+    private List<Book> categoryList;
 
     public static BookStoreFragment newInstance() {
         BookStoreFragment fragment = new BookStoreFragment();
@@ -22,13 +34,47 @@ public class BookStoreFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // generate test data
+        bookStoreList = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            categoryList = new ArrayList<>();
+            for (int j = 0; j < 4; j++) {
+                Book book = new Book("book" + j, "group" + i);
+                categoryList.add(book);
+            }
+            bookStoreList.add(categoryList);
+        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_store, container, false);
+        View view = inflater.inflate(R.layout.fragment_book_store, container, false);
+
+        //mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
+        //mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        mExpandableListView = (ExpandableListView) view.findViewById(R.id.el_book_store);
+        setUpExpandableListView();
+
+        return view;
+    }
+
+    private void setUpExpandableListView() {
+        mAdapter = new BookStoreExpandableListAdapter(this.getContext(), bookStoreList);
+        mExpandableListView.setAdapter(mAdapter);
+        for (int i = 0; i < bookStoreList.size(); i++) {
+            mExpandableListView.expandGroup(i);
+        }
+        mExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                return true;
+            }
+        });
     }
 
 }
